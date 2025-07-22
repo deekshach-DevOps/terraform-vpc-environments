a13dfe1f73098bc1140b40f23307ac30f58dcc32
deekshach-DevOps <deekshachoudhary1k93@gmail.com>
2025-07-22 15:30:58 +0200

    Update bucket name backend.tf

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket  = "dev-terraform-vpc"
+    bucket  = "azure-terraform-vpc"
     key     = "prod/vpc/terraform.tfstate"
     region  = "eu-south-1"
     encrypt = true
